d6aa05a51254a32b39e5511b344f0a394f84d8b2
yuwei <ywyw1234@163.com>
2018-08-07 21:01:08 +0800

    文件上传类

--- SpringBoot/src/main/java/com/comtop/controller/FileController.java
+++ SpringBoot/src/main/java/com/comtop/controller/FileController.java
@@ -6,18 +6,14 @@ import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.comtop.constants.SpringbootConstants;
 import com.comtop.model.JsonData;
-import com.comtop.model.User;
 
 @RestController
 public class FileController {
